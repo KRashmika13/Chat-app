fe6d53ea78d34817796ee202b53f4ca8cad00758
KRashmika13 <krashmika13@gmail.com>
2025-03-27 00:31:51 +0530

    implement authentication routes and MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './src/routes/auth.route.js';
+import { connectDB } from './src/lib/db.js';
 
+dotenv.config();
 const app = express();
 
-app.listen (5001, () => {
-    console.log('Server is running on port 5001');
+app.use ("/api/auth", authRoutes);
+const PORT = process.env.PORT || 5001;
+
+app.listen (PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
