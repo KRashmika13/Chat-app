43ffc5cb1bec3b86872927bcf7bd234e038ed601
KRashmika13 <krashmika13@gmail.com>
2025-04-03 19:42:38 +0530

    implement signup functionality with user validation and password hashing

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,36 @@
-export const signup = (req, res) => {
-    res.send('Signup Route')
+import User from "../models/users.moddle.js";
+import bcrypt from 'bcrypt';
+
+export const signup = async(req, res) => {
+    const { fullName, email, password} = req.body;
+    try{
+        if(password.length < 6){
+            return res.status(400).json({message: "Password should be at least 6 characters..."})
+        }
+
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({message: "User already exists..."})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if(newUser){
+
+        }else{
+            return res.status(400).json({message: "Invalid user data. Please try again."})
+        }
+
+    }catch{
+
+    }
 }
 
 export const login = (req, res) => {
