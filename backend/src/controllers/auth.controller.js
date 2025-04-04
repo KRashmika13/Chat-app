34d5ab43c71b4c6ca05ae48bf6c24b4cd62cc01c
KRashmika13 <krashmika13@gmail.com>
2025-04-04 09:11:50 +0530

    implement login functionality with user validation and error handling; enhance logout process

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,12 +6,12 @@ export const signup = async(req, res) => {
     const { fullName, email, password} = req.body;
     try{
         if(password.length < 6){
-            return res.status(400).json({message: "Password should be at least 6 characters..."})
+            return res.status(400).json({message: "Password should be at least 6 characters..."});
         }
 
         const user = await User.findOne({email});
         if(user){
-            return res.status(400).json({message: "User already exists..."})
+            return res.status(400).json({message: "User already exists..."});
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -35,19 +35,52 @@ export const signup = async(req, res) => {
                 token: generateToken(newUser._id, res)
             })
         }else{
-            return res.status(400).json({message: "Invalid user data. Please try again."})
+            return res.status(400).json({message: "Invalid user data. Please try again."});
         }
 
     }catch{
         console.log("Error in signup controller", error);
-        return res.status(500).json({message: "Internal server error"})
+        return res.status(500).json({message: "Internal server error"});
     }
 }
 
-export const login = (req, res) => {
-    res.send('Login Route')
+export const login = async(req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: "The email was not found. Please check your email and try again."});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(400).json({message: "The password is incorrect. Please try again."});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+            token: generateToken(user._id, res)
+        })
+    }catch{
+        console.log("Error in login controller", error);
+        return res.status(500).json({message: "Internal server error"});
+    }
 }
 
 export const logout = (req, res) => {
-    res.send('Logout Route')
+    try {
+        res.cookie("token", "", {maxAge: 0});
+        return res.status(200).json({message: "Logout successful"});
+    } catch {
+        console.log("Error in logout controller", error);
+        return res.status(500).json({message: "Internal server error"});
+    }
 }
